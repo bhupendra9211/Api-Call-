92b4605b1ebdb0e5388ff53837062a3493632b8e
bhupendra9211 <shahbhupendra9211@gmail.com>
2024-07-21 23:43:31 +0545

    implemented fetch api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+interface dataProps{
+  title:string;
+  id:number;
+}
+
+function App(){
+  const [data,setData] = useState<dataProps[]>([])
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(response => response.json())
+    // .then(response => console.log(response))
+    .then(response => setData(response as dataProps[]))
+  },[])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is Api call test</h1>
+      {/* {data.map((data) =>(data.title) )} */}
+      {data.map(data => <li key={data.id}>{data.title}</li> )}
     </div>
   );
 }
